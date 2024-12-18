da0d76370381a51d3999111206d8e17f6c629eb5
aj-osei <ajosei2004@gmail.com>
2024-12-18 11:52:30 -0600

    added in our wav file for example and made some try and catch methods for error handling!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello worlddddd!");
+        // Playing AUDIO with Java (.wav, .au, .aiff)
+
+        String filePath = "Jingle Bells - The Soundlings.wav";
+        File file = new File(filePath);
+
+        try(AudioInputStream audioStream = AudioSystem.getAudioInputStream(file)){
+
+        }
+        catch(UnsupportedAudioFileException e){
+            System.out.println("Audio file isn't supported! Please choose 'wav', '.au' or '.aiff'");
+        }
+        catch(IOException e){
+            System.out.println("Something went wrong!");
+        }
     }
 }
